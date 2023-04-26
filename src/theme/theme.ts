b14a3e800501b3c1da945cef8838700ae95d1135
Tiotedd <andrelsbrito2022@gmail.com>
2023-04-26 01:11:36 -0300

    Added default theme to the project and typed

---
+++ src/theme/theme.ts
@@ -0,0 +1,33 @@
+export default{
+  COLORS: {
+    WHITE: '#FFFFFF',
+
+    GREEN_DARK: '#639339',
+    GREEN_MID: '#CBE4B4',
+    GREEN_LIGHT: '#E5F0DB',
+
+    RED_DARK: '#BF3B44',
+    RED_MID: '#F3BABD',
+    RED_LIGHT: '#F4E6E7',
+
+    GRAY_700: '#FAFAFA',
+    GRAY_600: '#EFF0F0',
+    GRAY_500: '#DDDEDF',
+    GRAY_400: '#B9BBBC',
+    GRAY_300: '#5C6265',
+    GRAY_200: '#333638',
+    GRAY_100: '#1B1D1E'
+  },
+  FONT_FAMILY: {
+    REGULAR: 'NunitoSans_400Regular',
+    BOLD: 'NunitoSans_700Bold'
+  },
+  FONT_SIZE:{
+    XS_12: 12,
+    SM_14: 14,
+    MD_16: 16,
+    LG_18: 18,
+    XL_24: 24,
+    XXL_32: 32
+  }
+};
