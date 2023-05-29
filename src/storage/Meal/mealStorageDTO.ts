59359bd663c711285bcdd58b1f4b091ae0ff9e04
Tiotedd <andrelsbrito2022@gmail.com>
2023-05-28 23:39:22 -0300

    created data type of meals

---
+++ src/storage/Meal/mealStorageDTO.ts
@@ -0,0 +1,10 @@
+import { StatusStyleProps } from "../../components/Meal/styles"
+
+export type mealStorageDTO = {
+    id: number,
+    name: string,
+    description: string,
+    time: string,
+    date: string,
+    type: StatusStyleProps
+  }
