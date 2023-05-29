128112903acd53e4b507aecb771b659c90056a7e
Tiotedd <andrelsbrito2022@gmail.com>
2023-05-28 23:42:33 -0300

    added function to add new meal in storage

---
+++ src/storage/Meal/mealAddNewMeal.ts
@@ -0,0 +1,27 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { mealGetAll } from './mealGetAll'
+import { mealStorageDTO } from './mealStorageDTO'
+import { mealGetByKey } from './mealGetByKey'
+
+export async function mealAddNewMeal(newMeal: mealStorageDTO) {
+  try {
+    const storedMeal = await mealGetAll();
+    let isIdAlreadyExists = storedMeal.find((meal: mealStorageDTO) => meal.id === newMeal.id);
+
+    while (isIdAlreadyExists !== undefined) {
+      newMeal.id += 1;
+      isIdAlreadyExists = storedMeal.find((meal) => meal.id === newMeal.id);
+    }
+
+    const storaedMealByDate = await mealGetByKey(newMeal.date);
+    const updatedMealList = [newMeal, ...storaedMealByDate];
+    const storage = JSON.stringify(updatedMealList);
+
+    await AsyncStorage.setItem(newMeal.date, storage);
+
+    console.log('Nova refeição adicionada com sucesso!');
+  } catch (error) {
+    console.error('Erro ao adicionar nova refeição:', error);
+    // Trate o erro de acordo com suas necessidades.
+  }
+}
